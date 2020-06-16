00f5445d50da30ecb58c24a354885b6780d21804
Bar Margalit <bar.mrglt@gmail.com>
2020-06-16 16:27:09 +0300

    removed stage-0 as apears to be depracated

--- app/app.jsx
+++ app/app.jsx
@@ -1,8 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-
 ReactDOM.render(
-  <h1>boiler plate app</h1>,
+  <h1>Hello World with React!</h1>,
   document.getElementById('app')
 );
